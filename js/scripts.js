223a80ab1fc877ac50cfa46b44f624b3743c9a93
Lucy <lucyluney@gmail.com>
2018-07-30 15:05:34 -0300

    Calculo Tabuada Divisão

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 
 /**
- * Script que gerar o comportamente de gerar tabuada
+ * Script para gerar o comportamente de gerar tabuada(calculo e impressão na tela)
  */
 
-// campo para digitar o número
+// variavel (campoNumero) para armazenar numero digitado no tag html reservado para o numero
 var campoNumero = document.getElementById("numero");
 
 // elemento html para colocar o resultado dos calculos da tabuada
@@ -11,12 +11,14 @@ var resultadoTabuada = document.getElementById("resultado");
 
 // botão de Multiplicar
 var botaoMultiplicar = document.getElementById("botaoMultiplicar");
+var botaoDividir = document.getElementById("botaoDividir");
 
 // escutar evento de clique no botão para executar um script
-botaoMultiplicar.addEventListener("click", montarTabuada);
+botaoMultiplicar.addEventListener("click", montarTabuadaMultiplicacao);
+botaoDividir.addEventListener("click", montarTabuadaDivisao);
 
-// função para montar a tabuada
-function montarTabuada(){
+// função para montar a tabuada de Multiplicação
+function montarTabuadaMultiplicacao(){
 
     // o valor que vem do campo é texto (variavel do tipo string) no javascript
     var numeroText = campoNumero.value; 
@@ -44,3 +46,40 @@ function montarTabuada(){
 
     resultadoTabuada.innerHTML = htmlResultado;
 }
+
+
+// função para montar a tabuada de Divisão
+function montarTabuadaDivisao(){
+    
+        // o valor que vem do campo é texto (variavel do tipo string) no javascript
+        var numeroText = campoNumero.value; 
+        // console.log(typeof numeroText); // descomente essa linha para ver o tipo de variavel no CONSOLE
+        
+        // converter o texto para número inteiro
+        var numero = parseInt(numeroText);
+        // console.log(typeof numero); // descomente essa linha para ver o tipo de variavel no CONSOLE
+        
+        var htmlResultado = "";
+        htmlResultado = htmlResultado + "<ul>";
+
+        var numeroDividendo = numero;
+
+        // contar do número 1 até o número 
+        for (var contador = 1; contador <= 10; contador++){
+    
+            // resultado da conta
+            var total = (numeroDividendo / contador);
+           // var resto = (numeroDividendo % contador);
+           // if(resto == 0){
+                // gerar html para representar o calculo de um número especifico 
+                var htmlCalculoNumero = ("<li>" + numeroDividendo + " / " + contador  +  " = " + total + "</li>");
+                         
+           // adicionar o html gerado no final do html de resultado da tabuada
+            htmlResultado = htmlResultado + htmlCalculoNumero;
+        //  }
+           numeroDividendo = numeroDividendo + numero;      
+        }
+        htmlResultado = htmlResultado + "</ul>";
+    
+        resultadoTabuada.innerHTML = htmlResultado;
+    }
