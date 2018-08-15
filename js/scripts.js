723d4ea3431b72a82d3454b2e0c2c2c80bfa77cc
unknown <lucyluney@gmail.com>
2018-08-15 17:24:02 -0300

    Calculo Tabuada Subtração

--- js/scripts.js
+++ js/scripts.js
@@ -122,4 +122,34 @@ function montarTabuadaSoma(){
   }
   
   
+       // função para montar a tabuada de Subtração
+function montarTabuadaSubtracao(){
+    
+     // o valor que vem do campo é texto (variavel do tipo string) no javascript
+     var numeroText = campoNumero.value;
+     // console.log(typeof numeroText); // descomente essa linha para ver o tipo de variavel no CONSOLE
+    
+     // converter o texto para número inteiro
+     var numero = parseInt(numeroText);
+     // console.log(typeof numero); // descomente essa linha para ver o tipo de variavel no CONSOLE
+    
+     var htmlResultado = "";
+     htmlResultado = htmlResultado + "<ul>";
   
+      // contar do número 1 até o número
+     for (var contador = 1; contador <= 10; contador++){
+  
+         // resultado da conta
+         var total = (numero - contador);
+      
+             // gerar html para representar o calculo de um número especifico
+             var htmlCalculoNumero = ("<li>" + numero + " - " + contador  +  " = " + total + "</li>");
+                     
+        // adicionar o html gerado no final do html de resultado da tabuada
+         htmlResultado = htmlResultado + htmlCalculoNumero;
+      
+     }
+     htmlResultado = htmlResultado + "</ul>";
+  
+     resultadoTabuada.innerHTML = htmlResultado;
+  }
